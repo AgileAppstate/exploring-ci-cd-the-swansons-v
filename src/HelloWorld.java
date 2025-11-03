29c7b0d6f4b0f8d24bd33c997c48c40ed263f5af
GutknechtWD <gutknechtwd@appstate.edu>
2025-11-03 13:36:19 -0500

    Added a time stamp take two

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
-import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		LocalDate obj = LocalDate.now();
+		LocalTime obj = LocalTime.now();
 		System.out.println(obj);
 	}
 }
